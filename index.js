aed98da5fa55f119e9063020e315493d2cd947e6
cheonjae6 <cheonjae6@naver.com>
2022-04-18 21:02:39 +0900

    Update

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ bgm.src = "./bgm.mp3";
 window.addEventListener("mousemove", function() {
     bgm.play();
 });
+
+// function isBrowserCheck() {
+//     const agt = navigator.userAgent.toLowerCase();
+//     if (agt.indexOf("chrome") != -1) {
+//         window.addEventListener("mousemove", function() {
+//             bgm.play();
+//         });
+//     }
+//     else if (agt.indexOf("safari") != -1 || agt.indexOf("firefox") != -1 || agt.indexOf("opera") != -1) {
+//         bgm.play(true);
+//     }
+// }
+
+// const browser = isBrowserCheck();
